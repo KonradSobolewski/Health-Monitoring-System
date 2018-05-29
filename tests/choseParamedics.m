0772f11528034654cadb7cd85a94b3b9ab77e6db
Kinga Staszkiewicz <k.staszkiewicz@stud.elka.pw.edu.pl>
2018-05-29 20:18:51 +0200

    Szkielet funckcji wyboru ratowników

---
+++ tests/choseParamedics.m
@@ -0,0 +1,11 @@
+function [savers] = choseParamedics(savers,positions,inneed)
+
+indices = find(savers==1); 
+saver1 = indices(1);
+saver2 = indices(2);
+
+d1 = sqrt((positions(1,1) - positions(i,1))^2 + (positions(1,2) - positions(i,2))^2);
+d2 = 
+
+
+end
